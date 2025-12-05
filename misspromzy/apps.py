6496419a91a82fc458e881f4f9dc99a947c210a4
Praise Otuka <praiseotuka18@gmail.com>
2025-12-05 10:43:18 +0100

    Initial Django assignment

---
+++ misspromzy/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MisspromzyConfig(AppConfig):
+    name = 'misspromzy'
---
+++ misspromzy/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    content ={
+        "bio": "Welcome to my portfolio website! I'm excited to share my projects and experiences with you.",
+        "status": "Active",
+        "is_staff": True
+   }
+    return render(request, 'home.html', content)
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
